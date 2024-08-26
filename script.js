01ae3bf6eb2ced1e2c2700db8906eae454f87453
Zouares <gabrielssoaress02@gmail.com>
2024-08-26 10:18:51 -0300

    concertado

--- script.js
+++ script.js
@@ -19,20 +19,20 @@ function getCurrentTime() {
 
     const date = new Date();
 
-    let hora = date.getHours();
-    let minutos = date.getMinutes();
-    let segundos = date.getSeconds();
+    let hora = (date.getHours() < 10 ? '0' : '') + date.getHours();
+    let minutos = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+    let segundos = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
 
     return hora + ":" + minutos + ":" + segundos;
-
+     
 }
 
 function getCurrentDate() {
     
     const date = new Date();
     
-    let mes = date.getMonth() + 1;
-    let data = date.getDate();
+    let mes = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
+    let data = (date.getDate() < 10 ? '0' : '') + date.getDate();
     let ano = date.getFullYear();
 
     return data + "/" + mes  + "/" + ano;
