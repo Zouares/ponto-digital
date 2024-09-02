478cd29fdc9fe948cdfcb113b83317669da71f95
Zouares <gabrielssoaress02@gmail.com>
2024-09-02 20:01:15 -0300

    BOM DIA BOA TARDE BOA NOITE FULANO DE TAL

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position);
-});
-
 const diaSemana = document.getElementById("diasem");
 const dataAtual = document.getElementById("dia");
 const horaAtual = document.getElementById("hora");
+const greetingElement = document.getElementById("greeting");
 const botaoregistrar = document.getElementById("register-button");
 const dialogPonto = document.getElementById("dialog-ponto");
 const btnDialogFechar = document.getElementById("dialog-fechar");
@@ -12,13 +9,28 @@ const dialogData = document.getElementById("dialog-Data");
 const dialogHora = document.getElementById("dialog-Hora");
 
 function register() {
+    
+    dialogData.textContent = `Data: ${getCurrentDate()}`;
+    dialogHora.textContent = `Hora: ${getCurrentTime()}`;
     dialogPonto.showModal();
 }
 
 function updateContentHour() {
-    dataAtual.textContent = getCurrentDate();
     horaAtual.textContent = getCurrentTime();
+    dataAtual.textContent = getCurrentDate();
     diaSemana.textContent = getWeekDay();
+    greetingElement.textContent = getGreeting("Fulano de Tal");
+}
+
+function getGreeting(name) {
+    const hours = new Date().getHours();
+    if (hours < 12) {
+        return `Bom dia, ${name}`;
+    } else if (hours < 18) {
+        return `Boa tarde, ${name}`;
+    } else {
+        return `Boa noite, ${name}`;
+    }
 }
 
 function getCurrentTime() {
@@ -26,7 +38,7 @@ function getCurrentTime() {
     let hora = (date.getHours() < 10 ? '0' : '') + date.getHours();
     let minutos = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
     let segundos = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
-    return hora + ":" + minutos + ":" + segundos;
+    return `${hora}:${minutos}:${segundos}`;
 }
 
 function getCurrentDate() {
@@ -34,7 +46,7 @@ function getCurrentDate() {
     let mes = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
     let data = (date.getDate() < 10 ? '0' : '') + date.getDate();
     let ano = date.getFullYear();
-    return data + "/" + mes + "/" + ano;
+    return `${data}/${mes}/${ano}`;
 }
 
 function getWeekDay() {
@@ -48,14 +60,5 @@ btnDialogFechar.addEventListener("click", () => {
     dialogPonto.close();
 });
 
-dialogData.textContent = getCurrentDate();
-dialogHora.textContent = getCurrentTime();
-
-
-
 updateContentHour();
 setInterval(updateContentHour, 1000);
-
-console.log(getCurrentTime());
-console.log(getCurrentDate());
-console.log(getWeekDay());
